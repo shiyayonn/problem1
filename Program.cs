6978aa44c78ea8c24ebd695e97ede8ceae04fd5b
Jayson Concepcion <ysonconcepcion@gmail.com>
2021-12-06 17:24:49 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Problem1
+{
+    static void Main(string[] args)
+    {
+        int size = 0;
+        
+        size = Int32.Parse(Console.ReadLine().Trim());
+
+        int space = size;
+
+        for (int i = 1; i <= size; i++)
+        {
+            Console.WriteLine($"{new String(' ',--space)}{new String('#',i)}");
+        }
+    }
+}
